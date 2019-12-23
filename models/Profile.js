ba56b14726298e8ab9529e5cb3c4429b9f1f8649
Kusjay <kelechiuma@gmail.com>
2019-12-23 11:03:04 +0100

    Created the profile model

---
+++ models/Profile.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a profile name'],
+    trim: true,
+    maxlength: [50, 'profile name can not be more than 50 characters']
+  },
+  slug: String,
+  description: {
+    type: String,
+    require: [true, 'Please add a description'],
+    maxlength: [500, 'Description can not be more than 500 characters']
+  },
+  phone: {
+    type: String,
+    required: [true, 'Please add a phone number'],
+    maxlength: [20, 'Phone number can not be longer than 20 characters']
+  },
+  email: {
+    type: String,
+    match: [
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please add a valid email'
+    ]
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address']
+  },
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: ['Point']
+    },
+    coordinates: {
+      type: [Number],
+      index: '2dsphere'
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String
+  },
+  averageRating: {
+    type: Number,
+    min: [1, 'Rating must be at least 1'],
+    max: [10, 'Rating can not be more than 10']
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Profile', ProfileSchema);
