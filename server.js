80c61ac9b8e95701507875ff77dac79624440ed6
Kusjay <kelechiuma@gmail.com>
2019-12-23 06:11:56 +0100

    Connected to the database with mongoose

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Route files
-const profiles = require('./routes/profiles');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Route files
+const profiles = require('./routes/profiles');
 
 const app = express();
 
@@ -14,7 +18,14 @@ app.use('/api/v1/profiles', profiles);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
