d87a99e3155079f55149c75e28ea1bf2e7fba03f
Kusjay <kelechiuma@gmail.com>
2019-12-25 10:27:40 +0100

    Error handler middleware & custom error response class

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require('../utils/errorResponse');
 const Profile = require('../models/Profile');
 
 // @desc    Get all profiles
@@ -23,12 +24,16 @@ exports.getProfile = async (req, res, next) => {
     const profile = await Profile.findById(req.params.id);
 
     if (!profile) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Profile not found with id of ${req.params.id}`, 404)
+      );
     }
 
     res.status(200).json({ success: true, data: profile });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(
+      new ErrorResponse(`Profile not found with id of ${req.params.id}`, 404)
+    );
   }
 };
 
