f1ba01bbfe797c0251382764769035c3f89fefb5
Kusjay <kelechiuma@gmail.com>
2019-12-26 10:33:57 +0100

    Added an async await middleware

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,93 +1,74 @@
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const Profile = require('../models/Profile');
 
 // @desc    Get all profiles
 // @route   GET /api/v1/profiles
 // @access  Public
-exports.getProfiles = async (req, res, next) => {
-  try {
-    const profiles = await Profile.find();
+exports.getProfiles = asyncHandler(async (req, res, next) => {
+  const profiles = await Profile.find();
 
-    res
-      .status(200)
-      .json({ success: true, count: profiles.length, data: profiles });
-  } catch (err) {
-    next(err);
-  }
-};
+  res
+    .status(200)
+    .json({ success: true, count: profiles.length, data: profiles });
+});
 
 // @desc    Get single profile
 // @route   GET /api/v1/profiles/:id
 // @access  Public
-exports.getProfile = async (req, res, next) => {
-  try {
-    const profile = await Profile.findById(req.params.id);
+exports.getProfile = asyncHandler(async (req, res, next) => {
+  const profile = await Profile.findById(req.params.id);
 
-    if (!profile) {
-      return next(
-        new ErrorResponse(`Profile not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: profile });
-  } catch (err) {
-    next(err);
+  if (!profile) {
+    return next(
+      new ErrorResponse(`Profile not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: profile });
+});
 
 // @desc    Create new profile
 // @route   POST /api/v1/profiles
 // @access  Private
-exports.createProfile = async (req, res, next) => {
-  try {
-    const profile = await Profile.create(req.body);
+exports.createProfile = asyncHandler(async (req, res, next) => {
+  const profile = await Profile.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: profile
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: profile
+  });
+});
 
 // @desc    Update profile
 // @route   PUT /api/v1/profiles/:id
 // @access  Private
-exports.updateProfile = async (req, res, next) => {
-  try {
-    const profile = await Profile.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+exports.updateProfile = asyncHandler(async (req, res, next) => {
+  const profile = await Profile.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!profile) {
-      return next(
-        new ErrorResponse(`Profile not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: profile });
-  } catch (err) {
-    next(err);
+  if (!profile) {
+    return next(
+      new ErrorResponse(`Profile not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: profile });
+});
 
 // @desc    Delete profile
 // @route   DELETE /api/v1/profiles/:id
 // @access  Private
-exports.deleteProfile = async (req, res, next) => {
-  try {
-    const profile = await Profile.findByIdAndDelete(req.params.id);
+exports.deleteProfile = asyncHandler(async (req, res, next) => {
+  const profile = await Profile.findByIdAndDelete(req.params.id);
 
-    if (!profile) {
-      return next(
-        new ErrorResponse(`Profile not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: {} });
-  } catch (err) {
-    next(err);
+  if (!profile) {
+    return next(
+      new ErrorResponse(`Profile not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: {} });
+});
