e414636d3051f02d41a9b53b313f2d945fb76474
Kusjay <kelechiuma@gmail.com>
2019-12-28 12:59:46 +0100

    Single task and add task functionality

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,8 +1,9 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Task = require('../models/Task');
+const Profile = require('../models/Profile');
 
-// @desc    Get tasks
+// @desc    Get single task
 // @route   GET /api/v1/tasks
 // @route   GET /api/v1/profiles/:profileId/tasks
 // @access  Public
@@ -24,5 +25,47 @@ exports.getTasks = asyncHandler(async (req, res, next) => {
     success: true,
     count: tasks.length,
     data: tasks
+  });
+});
+
+// @desc    Get single task
+// @route   GET /api/v1/tasks/:id
+// @access  Public
+exports.getTask = asyncHandler(async (req, res, next) => {
+  const task = await Task.findById(req.params.id).populate({
+    path: 'profile',
+    select: 'name description'
+  });
+
+  if (!task) {
+    return next(new ErrorResponse(`No task with id of ${req.params.id}`), 404);
+  }
+
+  res.status(200).json({
+    success: true,
+    data: task
+  });
+});
+
+// @desc    Add task
+// @route   POST /api/v1/profiles/:profileId/tasks
+// @access  Private
+exports.addTask = asyncHandler(async (req, res, next) => {
+  req.body.profile = req.params.profileId;
+
+  const profile = await Profile.findById(req.params.profileId);
+
+  if (!profile) {
+    return next(
+      new ErrorResponse(`No profile with id of ${req.params.profileId}`),
+      404
+    );
+  }
+
+  const task = await Task.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: task
   });
 });
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { getTasks } = require('../controllers/tasks');
+const { getTasks, getTask, addTask } = require('../controllers/tasks');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getTasks);
+router
+  .route('/')
+  .get(getTasks)
+  .post(addTask);
+
+router.route('/:id').get(getTask);
 
 module.exports = router;
