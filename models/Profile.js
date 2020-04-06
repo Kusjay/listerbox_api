d627cb90ea520e67f4963de3189cdf96093b4d0b
Kusjay <kelechiuma@gmail.com>
2020-04-06 10:22:43 +0100

    Disable SELECT for bank name and account number on user profile

--- models/Profile.js
+++ models/Profile.js
@@ -9,85 +9,87 @@ const ProfileSchema = new mongoose.Schema(
       required: [true, 'Please add a profile name'],
       unique: true,
       trim: true,
-      maxlength: [50, 'Profile name can not be more than 50 characters']
+      maxlength: [50, 'Profile name can not be more than 50 characters'],
     },
     totalEarning: {
       type: Number,
-      default: 0
+      default: 0,
     },
     accountNumber: {
-      type: Number
+      type: Number,
+      select: false,
     },
     bankName: {
-      type: String
+      type: String,
+      select: false,
     },
     slug: String,
     description: {
       type: String,
       require: [true, 'Please add a description'],
-      maxlength: [500, 'Description can not be more than 500 characters']
+      maxlength: [500, 'Description can not be more than 500 characters'],
     },
     phone: {
       type: String,
       required: [true, 'Please add a phone number'],
-      maxlength: [20, 'Phone number can not be longer than 20 characters']
+      maxlength: [20, 'Phone number can not be longer than 20 characters'],
     },
     email: {
       type: String,
       match: [
         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        'Please add a valid email'
-      ]
+        'Please add a valid email',
+      ],
     },
     address: {
       type: String,
-      required: [true, 'Please add an address']
+      required: [true, 'Please add an address'],
     },
     location: {
       // GeoJSON Point
       type: {
         type: String,
-        enum: ['Point']
+        enum: ['Point'],
       },
       coordinates: {
         type: [Number],
-        index: '2dsphere'
+        index: '2dsphere',
       },
       formattedAddress: String,
       street: String,
       city: String,
       state: String,
       zipcode: String,
-      country: String
+      country: String,
     },
     photo: {
       type: String,
-      default: 'no-photo.jpg'
+      default: 'no-photo.jpg',
     },
     createdAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
     },
     user: {
       type: mongoose.Schema.ObjectId,
       ref: 'User',
-      required: true
-    }
+      required: true,
+    },
   },
   {
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
+    toObject: { virtuals: true },
   }
 );
 
 // Create profile slug from the name
-ProfileSchema.pre('save', function(next) {
+ProfileSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
 
 // Geocode & create location field
-ProfileSchema.pre('save', async function(next) {
+ProfileSchema.pre('save', async function (next) {
   const loc = await geocoder.geocode(this.address);
   this.location = {
     type: 'Point',
@@ -97,7 +99,7 @@ ProfileSchema.pre('save', async function(next) {
     city: loc[0].city,
     state: loc[0].stateCode,
     zipcode: loc[0].zipcode,
-    country: loc[0].countryCode
+    country: loc[0].countryCode,
   };
 
   // Do not save address in DB
@@ -106,7 +108,7 @@ ProfileSchema.pre('save', async function(next) {
 });
 
 // Cascade delete tasks when a profile is deleted
-ProfileSchema.pre('remove', async function(next) {
+ProfileSchema.pre('remove', async function (next) {
   await this.model('Task').deleteMany({ profile: this._id });
 });
 
@@ -115,7 +117,7 @@ ProfileSchema.virtual('tasks', {
   ref: 'Task',
   localField: '_id',
   foreignField: 'profile',
-  justOne: false
+  justOne: false,
 });
 
 module.exports = mongoose.model('Profile', ProfileSchema);
